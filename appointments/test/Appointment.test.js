b7b9357d6d5be19afc7a4f79066f52b11b133c81
Gilboot <ssenyonjogilbert@gmail.com>
2021-07-06 21:17:55 +0300

    Refactored call to ReactDOM to a separate render function

--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -12,16 +12,22 @@ describe('Appointment', () => {
     beforeEach(() => {
         container = document.createElement('section')
     })
+
+    const render = component => ReactDOM.render(component, container)
+
     // test
     it('renders the current first name', () => {
+        // Arrance
         customer = { firstName: 'Ashley Jackson' }
-        ReactDOM.render(<Appointment customer ={customer} />, container)
+        // Act
+        render(<Appointment customer ={customer} />)
+        // Assert
         expect(container.textContent).toMatch('Ashley')
     })
 
     it('renders another current first name', () => {
         customer = { firstName: 'Mary' }
-        ReactDOM.render(<Appointment customer ={customer} />, container)
+        render(<Appointment customer ={customer} />)
         expect(container.textContent).toMatch('Mary')
     })
 })
