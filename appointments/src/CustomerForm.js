3c28b2275f62adc07e4c63fd1b8b488bb569bc0c
Gilboot <ssenyonjogilbert@gmail.com>
2021-07-08 07:48:08 +0300

    test renders the first name field as a text box'

---
+++ appointments/src/CustomerForm.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+export const CustomerForm = () => (
+    <form id="customer">
+        <input 
+            type="text"
+            name="firstName"
+            />
+    </form>
+)
+
+
